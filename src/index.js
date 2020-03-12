557150bd7999f7fd57a23847f0b28a73323c8b85
unknown <capsors22@gmail.com>
2020-03-12 14:17:02 +0200

    Fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    let strR = String(n).replace(/\D/g,'').replace(/0$/g,'');
+    let result = strR.split('').reverse().join('')
+    return Number(result);
 }
